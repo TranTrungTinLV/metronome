fe061af5ee58279cd2e2f156bee9335d01136a0d
TranTrungTinLV <74409780+TranTrungTinLV@users.noreply.github.com>
2022-11-02 18:35:26 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'page.dart';
-
+import 'package:flutter/services.dart';
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+  ]);
   runApp(BMI());
 }
 
